fdc124c4d26d9db40c5cd0f160057e2023674105
Carlos-93 <carlos93.bcn@hotmail.com>
2024-04-09 16:00:35 +0200

    Actualizar estilos globales y eliminar archivos de diseño

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 // Página principal de la aplicación
 export default function Home() {
 
@@ -5,7 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen justify-center items-center flex-col gap-10">
       <h1 className="text-5xl">Proyecto Final</h1>
-      <h2 className="text-3xl">Next.js 14</h2>
+      <Image src="/next.svg" alt="Next.js Logo" width={150} height={150} />
     </main>
   );
 }
