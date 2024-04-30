f89e25df416dbc5b3abc13f271d0b06f4b024af9
Carlos-93 <carlos93.bcn@hotmail.com>
2024-04-30 17:35:50 +0200

    Update

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
         <React.Fragment>
             <header className="bg-white fixed top-0 w-full flex justify-end items-center p-4 px-14">
                 <div className="flex gap-5">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user-square" width="50" height="50" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000" fill="none" strokeLinecap="round" stroke-linejoin="round">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user-square" width="50" height="50" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000" fill="none" strokeLinecap="round" strokeLinejoin="round">
                         <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                         <path d="M9 10a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
                         <path d="M6 21v-1a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v1" />
--- src/components/layouts/sidebar.tsx
+++ src/components/layouts/sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar() {
                 <Image src="/assets/images/logo.png" alt="Logo Nexus Estudiantil" width={260} height={250} />
                 <ul className="flex flex-col gap-3">
                     <div className="my-4">
-                        <input type="search" placeholder="Buscar curso..." className="pl-4 p-2 w-full bg-gray-700 text-white rounded" />
+                        <input type="search" placeholder="Buscar curso..." className="pl-4 p-2 w-full bg-gray-700 text-white rounded" id="search-course" name="search-course" />
                     </div>
                     <li>
                         <a href="#" className="block p-2 pl-4 bg-gray-700 hover:bg-gray-900 hover:text-teal-500 transition-all ease-in-out duración-300 rounded hover:translate-x-2">
@@ -15,7 +15,13 @@ export default function Sidebar() {
                         </a>
                     </li>
                     <li>
-                        <a href="#" className="block p-2 pl-4 bg-gray-700 hover:bg-gray-900 hover:text-teal-500 transition-all ease-in-out duración-300 rounded hover:translate-x-2">
+                        <a href="#" className="flex gap-2 p-2 pl-4 bg-gray-700 hover:bg-gray-900 hover:text-teal-500 transition-all ease-in-out duración-300 rounded hover:translate-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-book"><path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                                <path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" />
+                                <path d="M3 6l0 13" /><path d="M12 6l0 13" />
+                                <path d="M21 6l0 13" />
+                            </svg>
                             ESO
                         </a>
                         <ul className="ml-10 flex flex-col gap-2 mt-2 text-sm">
@@ -42,7 +48,12 @@ export default function Sidebar() {
                         </ul>
                     </li>
                     <li>
-                        <a href="#" className="block p-2 pl-4 bg-gray-700 hover:bg-gray-900 hover:text-teal-500 transition-all ease-in-out duración-300 rounded hover:translate-x-2">
+                        <a href="#" className="flex gap-2 p-2 pl-4 bg-gray-700 hover:bg-gray-900 hover:text-teal-500 transition-all ease-in-out duración-300 rounded hover:translate-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-school">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M22 9l-10 -4l-10 4l10 4l10 -4v6" />
+                                <path d="M6 10.6v5.4a6 3 0 0 0 12 0v-5.4" />
+                            </svg>
                             Bachillerato
                         </a>
                         <ul className="ml-10 flex flex-col gap-2 mt-2 text-sm">
@@ -59,7 +70,12 @@ export default function Sidebar() {
                         </ul>
                     </li>
                     <li>
-                        <a href="#" className="block p-2 pl-4 bg-gray-700 hover:bg-gray-900 hover:text-teal-500 transition-all ease-in-out duration-300 rounded hover:translate-x-2">
+                        <a href="#" className="flex gap-2 p-2 pl-4 bg-gray-700 hover:bg-gray-900 hover:text-teal-500 transition-all ease-in-out duration-300 rounded hover:translate-x-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-camera">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
+                                <path d="M9 13a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+                            </svg>
                             Marketing
                         </a>
                         <ul className="ml-10 flex flex-col gap-2 mt-2 text-sm">
